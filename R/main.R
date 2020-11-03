bc55d67e9cb8f3d862f78982666799f6e9acdb6a
zach <wz.horton@gmail.com>
2020-11-03 02:55:37 -0800

    Version 0.2.0 > complete multi-group function and documentation + bug fix

--- R/main.R
+++ R/main.R
@@ -26,7 +26,7 @@ multi_group_fda <- function(data_list, p = 20, niter = 10000, nburn = 1000){
   lasts <- cumsum(sapply(data_list, ncol))
   time <- seq(0,1,len = nrow(data_list[[1]]))
   out <- .g2g_fda(as.matrix(as.data.frame(data_list)), lasts-1, time, p, niter, nburn)
-  names(out) <- c("tau2", "sig2","beta_grp", "beta_curve")
+  names(out) <- c("tau2", "sig2","beta_grp", "beta_crv")
   return(out)
 }
 
